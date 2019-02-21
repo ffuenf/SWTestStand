b719755935362dc68fe545806c34285fb4f0c020
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2019-02-21 13:43:05 +0100

    fix build phase

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 set -e
 set -x
 
-if [ "$TRAVIS_TAG" != "" ]; then
-  RELEASEDIR=`mktemp -d /tmp/${PLUGIN_NAME}-${TRAVIS_TAG}.XXXXXXXX`
-  echo "Using release directory ${RELEASEDIR}"
-  cd ${BUILDENV}/shopware/custom/plugins/${PLUGIN_NAME}
+if [ $TRAVIS_TAG != '' ]; then
+  RELEASEDIR=`mktemp -d /tmp/$PLUGIN_NAME-$TRAVIS_TAG.XXXXXXXX`
+  echo "Using release directory $RELEASEDIR"
+  cd ${BUILDENV}/shopware/custom/plugins/$PLUGIN_NAME
   rsync -av \
     --exclude='build/' \
     --exclude='.travis/' \
@@ -14,12 +14,12 @@ if [ "$TRAVIS_TAG" != "" ]; then
     --exclude='.gitignore' \
     --exclude='.gitmodules' \
     --exclude='tests' \
-    . ${RELEASEDIR}/${PLUGIN_NAME}/
-  cd ${RELEASEDIR}/
-  zip -r ${PLUGIN_NAME}-${TRAVIS_TAG}.zip ${PLUGIN_NAME}
-  mv ${PLUGIN_NAME}-${TRAVIS_TAG}.zip $WORKSPACE
-  tar -czf ${PLUGIN_NAME}-${TRAVIS_TAG}.tar.gz ${PLUGIN_NAME}
-  mv ${PLUGIN_NAME}-${TRAVIS_TAG}.tar.gz $WORKSPACE
-  rm -rf ${RELEASEDIR}
-  echo "Bundled release ${TRAVIS_TAG}"
+    . $RELEASEDIR/$PLUGIN_NAME/
+  cd $RELEASEDIR/
+  zip -r $PLUGIN_NAME-$TRAVIS_TAG.zip $PLUGIN_NAME
+  mv $PLUGIN_NAME-${TRAVIS_TAG}.zip $WORKSPACE
+  tar -czf $PLUGIN_NAME-$TRAVIS_TAG.tar.gz $PLUGIN_NAME
+  mv $PLUGIN_NAME-$TRAVIS_TAG.tar.gz $WORKSPACE
+  rm -rf $RELEASEDIR
+  echo "Bundled release $TRAVIS_TAG"
 fi
