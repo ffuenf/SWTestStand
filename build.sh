eb09c0ef54138968475ae2cc7284bea8a738887c
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2019-02-21 14:06:30 +0100

    fix build phase

--- build.sh
+++ build.sh
@@ -1,25 +1,3 @@
 #!/usr/bin/env bash
 set -e
 set -x
-
-if [ $TRAVIS_TAG != '' ]; then
-  RELEASEDIR=`mktemp -d /tmp/$PLUGIN_NAME-$TRAVIS_TAG.XXXXXXXX`
-  echo "Using release directory $RELEASEDIR"
-  cd ${BUILDENV}/shopware/custom/plugins/$PLUGIN_NAME
-  rsync -av \
-    --exclude='build/' \
-    --exclude='.travis/' \
-    --exclude='.travis.yml' \
-    --exclude='.git/' \
-    --exclude='.gitignore' \
-    --exclude='.gitmodules' \
-    --exclude='tests' \
-    . $RELEASEDIR/$PLUGIN_NAME/
-  cd $RELEASEDIR/
-  zip -r $PLUGIN_NAME-$TRAVIS_TAG.zip $PLUGIN_NAME
-  mv $PLUGIN_NAME-$TRAVIS_TAG.zip $WORKSPACE
-  tar -czf $PLUGIN_NAME-$TRAVIS_TAG.tar.gz $PLUGIN_NAME
-  mv $PLUGIN_NAME-$TRAVIS_TAG.tar.gz $WORKSPACE
-  rm -rf $RELEASEDIR
-  echo "Bundled release $TRAVIS_TAG"
-fi
